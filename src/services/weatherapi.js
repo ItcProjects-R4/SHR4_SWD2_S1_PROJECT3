c7141aa2820ef688ee8a1350c8cea8907d75b7e7
Aya Shoman <ayaabdelhamid567@gmail.com>
2026-06-26 17:13:02 +0300

    Refactor weather API key and clean comments

--- src/services/weatherapi.js
+++ src/services/weatherapi.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
-
-const API_KEY = 'ba35e8cf316a640b56b271658c8a8634'; // مفتاح API مجاني وشغال جاهز لمشروعك
+const API_KEY = 'ba35e8cf316a640b56b271658c8a8634';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
-
 export const getWeatherData = async (city) => {
   try {
     const response = await axios.get(BASE_URL, {
       params: {
         q: city,
         appid: API_KEY,
-        units: 'metric', // عشان درجات الحرارة تطلع بالـ Celsius مئوية
+        units: 'metric', 
       },
     });
     return response.data;
@@ -18,7 +16,7 @@ export const getWeatherData = async (city) => {
     throw error;
   }
 };
-// ضيفي الدالة دي في ملف weatherapi.js واعملي لها export
+
 export const getForecastData = async (city) => {
   const API_KEY = "ba35e8cf316a640b56b271658c8a8634"; 
   const BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
